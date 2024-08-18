ca9146d9b320c758b8c8a711721d5b1790d54a62
JacMcQ <jacqlyn.mcquade@icloud.com>
2024-08-18 13:43:23 -0600

    deleted unecessary files from testing, debug  for adding css, typo in main.handlebars. Separated login and Sign-up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,4 +31,14 @@ router.get("/login", (req, res) => {
   }
   res.render("login");
 });
+// In controllers/home-routes.js
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
+});
+
 module.exports = router;
