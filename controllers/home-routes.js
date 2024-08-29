844d501e200dbc2789a24a7c2784ecbf9ca09c76
JacMcQ <jacqlyn.mcquade@icloud.com>
2024-08-29 17:35:06 -0600

    updated routes for middleware auth

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const { Blog, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
+
 
 router.get("/", async (req, res) => {
   try {
@@ -15,7 +17,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/post/:id", async (req, res) => {
+router.get("/post/:id", withAuth, async (req, res) => {
   try {
     const dbBlogData = await Blog.findByPk(req.params.id, {
       include: [
